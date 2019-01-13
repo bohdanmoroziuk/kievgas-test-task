c340fd00a33e0bef19751aa054c1d92e92cb57fb
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2019-01-13 16:40:40 +0200

    add components: Navbar, Chart, Users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChartComponent } from './chart/chart.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ChartComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
