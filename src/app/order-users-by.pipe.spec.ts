965eaa4a27a95a6cc5955974884bea1dfe08a7d0
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2019-01-13 17:19:11 +0200

    create orderUsersBy pipe

---
+++ src/app/order-users-by.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { OrderUsersByPipe } from './order-users-by.pipe';
+
+describe('OrderUsersByPipe', () => {
+  it('create an instance', () => {
+    const pipe = new OrderUsersByPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
