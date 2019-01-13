a86ad19c416c62304c236e9ff2b5c43361481d7b
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2019-01-13 16:50:29 +0200

    configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ChartComponent } from './chart/chart.component';
+import { UsersComponent } from './users/users.component';
+
+const routes: Routes = [
+  { path: 'chart', component: ChartComponent },
+  { path: 'users', component: UsersComponent },
+
+  { path: '', redirectTo: '/chart', pathMatch: 'full' },
+  { path: '**', redirectTo: '/chart' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
